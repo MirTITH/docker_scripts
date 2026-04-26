05193f05e122bc8540ca9758093d9ae0989efd17
Yang XIE <1023515576@qq.com>
2026-04-26 22:26:34 +0800

    feat: 更新 Powerlevel10k 配置，启用 os_icon 并禁用 vcs 和 time

--- docker_files/dotfiles/home/.p10k.zsh
+++ docker_files/dotfiles/home/.p10k.zsh
@@ -30,9 +30,9 @@
 
   # The list of segments shown on the left. Fill it with the most important segments.
   typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
-    # os_icon               # os identifier
+    os_icon               # os identifier
     dir                     # current directory
-    vcs                     # git status
+    # vcs                     # git status
     prompt_char             # prompt symbol
   )
 
@@ -101,7 +101,7 @@
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
     per_directory_history   # Oh My Zsh per-directory-history local/global indicator
     # cpu_arch              # CPU architecture
-    time                    # current time
+    # time                    # current time
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
